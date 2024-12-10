a4217f7b0aea66f5e9e6d558d76e050a424cf108
linbinbin <lbbbboom@163.com>
2024-12-10 16:18:53 +0800

    feat: ✨ 新增commitlint配置

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
